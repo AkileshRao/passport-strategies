4e8a5c727042fd452607a93f9e550278b133e1f9
akileshrao <akileshrao19@gmail.com>
2021-11-01 07:31:53 +0530

    Basic setup final

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors());
+
+app.post('/register', (req, res) => {
+    console.log('Register post request');
+})
 
 app.post('/login', (req, res) => {
     console.log('Login post request');
