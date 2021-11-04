4a6dcc02c60ba7c96f0d8474281cce030712e5bb
akileshrao <akileshrao19@gmail.com>
2021-11-04 07:33:46 +0530

    Passport JWT strategy implemented

--- app.js
+++ app.js
@@ -1,17 +1,83 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { hashSync, compareSync } = require('bcrypt');
+const UserModel = require('./config/database');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
+app.use(passport.initialize());
+
+require('./config/passport')
 
 app.post('/register', (req, res) => {
-    console.log('Register post request');
+    const user = new UserModel({
+        username: req.body.username,
+        password: hashSync(req.body.password, 10)
+    })
+
+    user.save().then(user => {
+        res.send({
+            success: true,
+            message: "User created successfully.",
+            user: {
+                id: user._id,
+                username: user.username
+            }
+        })
+    }).catch(err => {
+        res.send({
+            success: false,
+            message: "Something went wrong",
+            error: err
+        })
+    })
 })
 
 app.post('/login', (req, res) => {
-    console.log('Login post request');
+    UserModel.findOne({ username: req.body.username }).then(user => {
+        //No user found
+        if (!user) {
+            return res.status(401).send({
+                success: false,
+                message: "Could not find the user."
+            })
+        }
+
+        //Incorrect password
+        if (!compareSync(req.body.password, user.password)) {
+            return res.status(401).send({
+                success: false,
+                message: "Incorrect password"
+            })
+        }
+
+        const payload = {
+            username: user.username,
+            id: user._id
+        }
+
+        const token = jwt.sign(payload, "Random string", { expiresIn: "1d" })
+
+        return res.status(200).send({
+            success: true,
+            message: "Logged in successfully!",
+            token: "Bearer " + token
+        })
+    })
+})
+
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.status(200).send({
+        success: true,
+        user: {
+            id: req.user._id,
+            username: req.user.username,
+        }
+    })
 })
 
 app.listen(5000, () => console.log("Listening to port 5000"));
--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,22 @@
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const opts = {}
+const UserModel = require('./database')
 const passport = require('passport')
-const JWTStrategy = require('passport-jwt').Strategy;
 
-passport.use(new JWTStrategy())
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'Random string';
+
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    UserModel.findOne({ id: jwt_payload.id }, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
